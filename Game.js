5bf4538e2e61db4b9c4a29b95798404a0a3a748c
Nikos <nkatsikanis@gmail.com>
2015-01-02 17:24:12 +0000

    Initial rendering of Cores and positioning

--- Game.js
+++ Game.js
@@ -1,14 +1,17 @@
-import Core from 'gameUnits/Core'
+import {Core} from 'gameUnits/Core'
 
 export class Game {
 	constructor() {
 		var self = this;
-		this.cores = [];
 		this.numCores = 6;
+		this.defaultY = 1;
 		// Load BABYLON 3D engine
 		this.canvas = document.getElementById("glcanvas");
 		this.engine = new BABYLON.Engine(this.canvas, true);
-		this.scene = this.createScene();
+		this.scene = new BABYLON.Scene(this.engine);
+		this.initScene();
+		this.cores = this.createInitialPlayerUnits();
+		this.postionCircular(this.cores);
 
 		this.engine.runRenderLoop(function () {
 			self.scene.render();
@@ -18,33 +21,29 @@ export class Game {
 		});
 	}
 
-	createScene() {
-		// This creates a basic Babylon Scene object (non-mesh)
-		var scene = new BABYLON.Scene(this.engine);
+	initScene() {
 		// This creates and positions a free camera (non-mesh)
-		var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+		var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), this.scene);
 		// This targets the camera to scene origin
 		camera.setTarget(BABYLON.Vector3.Zero());
 		// This attaches the camera to the canvas
 		camera.attachControl(this.canvas, true);
 		// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-		var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+		var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this.scene);
 
 		// Default intensity is 1. Let's dim the light a small amount
 		light.intensity = 0.7;
-		this.cores = this.createInitialPlayerUnits(scene);
-		this.postionCircular(this.cores);
 		// Move the sphere upward 1/2 its height
-		sphere.position.y = 1;
 		// Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-		var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
-		return scene;
+		var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, this.scene);
 	}
 
-	createInitialPlayerUnits(scene) {
-		var cores;
+	createInitialPlayerUnits() {
+		var cores = [];
 		for (var i = 0; i < this.numCores; i++) {
-			var sphere = new Core();
+			var core = new Core(this.scene);
+			core.sphere.position.y = this.defaultY;
+			cores.push(core)
 		}
 		return cores;
 	}
@@ -52,15 +51,14 @@ export class Game {
 	/**
 	 positions an array of objects on the edge of a circle equally spaced
 	 */
-	postionCircular(arr) {
+	postionCircular(cores) {
 		"use strict";
-		for (var i = 0; i < arr.length; i++) {
-			var angleDeg = i * (360 / arr.length);
+		for (var i = 0; i < cores.length; i++) {
+			var angleDeg = i * (360 / cores.length);
 			var angleRad = (angleDeg / 360) * 2 * Math.PI;
-			var customVector = new BABYLON.Vector2(-Math.cos(radAngle), -Math.sin(radAngle));
-			arr[i].position = customVector();
+			var customVector = new BABYLON.Vector3(-Math.cos(angleRad), this.defaultY, -Math.sin(angleRad));
+			cores[i].sphere.position = customVector;
 		}
-		return arr;
 	}
 
 ;
