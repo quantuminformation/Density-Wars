0e9a2a5c1e33d35c87ea41cbdc29564c05f1788c
Nikos Katsikanis <nKatsikanis@gmail.com>
2015-12-25 21:00:27 +0000

    Add unit selection

--- lib/gameUnits/Core.ts
+++ lib/gameUnits/Core.ts
@@ -1,15 +1,20 @@
 import UnitCommand from '../utils/UnitCommand';
 import Common from "../Common";
 import {IGameUnit} from "../gameUnits/IGameUnit";
+import ActionEvent = BABYLON.ActionEvent;
 
 
 /**
  *
  * User controllable unit
  */
+
+//todo I don't like this
+var self;
+
 export default class Core implements IGameUnit {
   mesh:BABYLON.Mesh;
-  isSelected:boolean; //selected units can receive new commands
+  isSelected:boolean = false; //selected units can receive new commands
   modifiers:Array<any>;
   hitPoints:number = 10;
   click:(e:MouseEvent)=>void;
@@ -22,6 +27,8 @@ export default class Core implements IGameUnit {
   currentCommand:UnitCommand; //poss move to Igameunit
 
   constructor(scene, isSelected = false) {
+    self = this; //for use ExecuteCodeAction callbacks
+
     this.mesh = BABYLON.Mesh.CreateSphere("sphere1", 8, Common.MEDIUM_UNIT_SIZE, scene);
     this.isSelected;//selected units receive commands
     this.modifiers = [];//powerups,shields etc
@@ -36,15 +43,17 @@ export default class Core implements IGameUnit {
 
     this.mesh.actionManager = new BABYLON.ActionManager(scene);
 
-    this.mesh.actionManager.registerAction(
-      new BABYLON.SetValueAction(BABYLON.ActionManager.OnPickTrigger, this.mesh.material, "wireframe", true)
-      )
-      .then(
-        new BABYLON.DoNothingAction()
-      );
+    //show bounding box for selected elements
+    this.mesh.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, this.select));
+
+    //show user where mouse is hovering over
     this.mesh.actionManager.registerAction(new BABYLON.SetValueAction(BABYLON.ActionManager.OnPointerOverTrigger, this.mesh.material, "diffuseColor", BABYLON.Color3.White()));
     this.mesh.actionManager.registerAction(new BABYLON.SetValueAction(BABYLON.ActionManager.OnPointerOutTrigger, this.mesh.material, "diffuseColor", this.material.diffuseColor));
+  }
 
+  select(e:ActionEvent) {
+    self.isSelected = true;
+    e.meshUnderPointer.showBoundingBox = true;
   }
 }
 
